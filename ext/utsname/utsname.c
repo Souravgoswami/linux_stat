a986ef98396bced9c8a3b9ab84248c4cd479db08
Sourav Goswami <souravgoswami@protonmail.com>
2020-12-01 21:04:36 +0530

    Improved performance of utsname.c

--- ext/utsname/utsname.c
+++ ext/utsname/utsname.c
@@ -1,19 +1,26 @@
 #include <sys/utsname.h>
 #include "ruby.h"
 
+static struct utsname buf ;
+static VALUE machine, nodename ;
+
+void init_buf() {
+	short status = uname(&buf) ;
+	machine = status < 0 ? rb_str_new_cstr("") : rb_str_new_cstr(buf.machine) ;
+	nodename = status < 0 ? rb_str_new_cstr("") : rb_str_new_cstr(buf.nodename) ;
+}
+
 static VALUE getMachine(VALUE obj) {
-	struct utsname buf ;
-	if ( uname(&buf) < 0 ) return rb_str_new_cstr("") ;
-	return rb_str_new_cstr(buf.machine) ;
+	return machine ;
 }
 
 static VALUE getNodename(VALUE obj) {
-	struct utsname buf ;
-	if (uname(&buf) < 0 ) return rb_str_new_cstr("") ;
-	return rb_str_new_cstr(buf.nodename) ;
+	return nodename ;
 }
 
 void Init_utsname() {
+	init_buf() ;
+
 	VALUE _uname = rb_define_module("Uname") ;
 	rb_define_module_function(_uname, "machine", getMachine, 0) ;
 	rb_define_module_function(_uname, "nodename", getNodename, 0) ;

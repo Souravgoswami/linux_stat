cabf31e51c9a8cc3ec02744bd70d7e612d84d1ef
Sourav Goswami <souravgoswami@protonmail.com>
2020-12-08 19:22:24 +0530

    Added Module PrettifyBytes

--- lib/linux_stat.rb
+++ lib/linux_stat.rb
@@ -1,20 +1,26 @@
+# Independed and LinuxStat specific unrelated modules
 require "linux_stat/version"
+require 'linux_stat/prettify_bytes'
+
+# Independed and LinuxStat related modules
 require "linux_stat/battery"
 require "linux_stat/bios"
 require "linux_stat/cpu"
 require "linux_stat/memory"
 require "linux_stat/net"
-
-require 'linux_stat/utsname'
-require "linux_stat/os"
-
 require "linux_stat/process"
 require "linux_stat/swap"
 
+# LinuxStat::Uname dependent modules
+require 'linux_stat/utsname'
+require "linux_stat/os"
+
+# LinuxStat::FS dependent modules
 require "linux_stat/fs_stat"
 require "linux_stat/filesystem"
 require "linux_stat/mounts"
 
+# LinuxStat::Sysconf dependent modules
 require "linux_stat/sysconf"
 require "linux_stat/kernel"
 require "linux_stat/process_info"
---
+++ lib/linux_stat/prettify_bytes.rb
@@ -0,0 +1,67 @@
+module LinuxStat
+	module PrettifyBytes
+		class << self
+			# Converts a number to decimal byte units and outputs with the metric prefix
+			# For example,
+			#
+			# LinuxStat::PrettifyBytes.convert_decimal(1000)    # => "1.0 kilobyte"
+			# LinuxStat::PrettifyBytes.convert_decimal(1000 ** 3)    # => "1.0 gigabyte"
+			# LinuxStat::PrettifyBytes.convert_decimal(1024 ** 3)   # => "1.07 gigabytes"
+			def convert_decimal(n)
+				@@d_units ||= %W(#{''} kilo mega giga tera peta exa zetta)
+					.map.with_index { |x, i| [x, 1000.**(i + 1)] }
+				unit = @@d_units.find { |x| n < x[1] } || ['yotta'.freeze, 10 ** 27]
+
+				converted = n.fdiv(unit[1] / 1000).round(2)
+				"#{converted} #{unit[0]}byte#{?s.freeze if converted != 1}"
+			end
+
+			# Converts a number to binary byte units and outputs with the IEC prefix
+			# For example,
+			#
+			# LinuxStat::PrettifyBytes.convert_binary(1000)    # => "1000.0 bytes"
+			# LinuxStat::PrettifyBytes.convert_binary(1000 ** 3)    # => "953.67 mebibytes"
+			# LinuxStat::PrettifyBytes.convert_binary(1024 ** 3)   # => "1.0 gibibyte"
+			def convert_binary(n)
+				@@b_units ||= %W(#{''} kibi mebi gibi tebi pebi exbi zebi)
+					.map.with_index { |x, i| [x, 1024.**(i + 1)] }
+				unit = @@b_units.find { |x| n < x[1] } || ['yobi'.freeze, 10 ** 27]
+
+				converted = n.fdiv(unit[1] / 1024).round(2)
+				"#{converted} #{unit[0]}byte#{?s.freeze if converted != 1}"
+			end
+
+			# Converts a number to decimal byte units
+			# For example,
+			#
+			# LinuxStat::PrettifyBytes.convert_short_decimal(1000)    # => "1.0 kB"
+			# LinuxStat::PrettifyBytes.convert_short_decimal(1000 ** 3)   # => "1.0 GB"
+			# LinuxStat::PrettifyBytes.convert_short_decimal(1024 ** 3)    # => "1.07 GB"
+			def convert_short_decimal(n)
+				@@sd_units ||= %W(#{''} k M G T P E Z)
+					.map.with_index { |x, i| [x, 1000.**(i + 1)] }
+				unit = @@sd_units.find { |x| n < x[1] } || [?Y.freeze, 10 ** 27]
+
+				converted = n.fdiv(unit[1] / 1000).round(2)
+				"#{converted} #{unit[0]}B"
+			end
+
+			# Converts a number to binary byte units
+			# For example,
+			#
+			# LinuxStat::PrettifyBytes.convert_short_binary(1000)    # => "1000 B"
+			# LinuxStat::PrettifyBytes.convert_short_binary(1000 ** 3)   # => "953.67 MiB"
+			# LinuxStat::PrettifyBytes.convert_short_binary(1024 ** 3)    # => "1.0 GiB"
+			def convert_short_binary(n)
+				return "#{n} B" if n < 1024
+
+				@@sb_units ||= %W(#{''} K M G T P E Z)
+					.map.with_index { |x, i| [x, 1024.**(i + 1)] }
+				unit = @@sb_units.find { |x| n < x[1] } || [?Y.freeze, 1024 ** 9]
+
+				converted = n.fdiv(unit[1] / 1024).round(2)
+				"#{converted} #{unit[0]}iB"
+			end
+		end
+	end
+end

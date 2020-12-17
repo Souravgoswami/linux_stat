480ca6df5f729f2a4f255f0321e288adedad2fbc
Sourav Goswami <souravgoswami@protonmail.com>
2020-12-17 14:00:47 +0530

    Updated linuxstat.rb

--- exe/linuxstat.rb
+++ exe/linuxstat.rb
@@ -7,6 +7,46 @@ rescue LoadError
 	abort "The Gem needs to be installed before this test can be run!"
 end
 
+# Gradient colour to strings
+class String
+	def colourize(colour = 1, flip: false)
+		colours, line_length = [], -1
+		temp = ''
+
+		each_line do |c|
+			n, i = c.length, -1
+
+			if line_length != n
+				step, line_length = 255.0./(n), n
+				colours.clear
+
+				while (i += 1) < n
+					l = i.*(step)
+					colours.<<(
+						case colour
+							when 0 then [ l.*(2).to_i.clamp(0, 255), l.to_i.clamp(0, 255), 255.-(l).to_i.clamp(0, 255) ]
+							when 1 then [ 255, 255.-(l).to_i.clamp(0, 255), l.to_i.clamp(0, 255) ]
+							when 2 then [ l.to_i.clamp(0, 255), 255.-(l).to_i.clamp(0, 255), l.to_i.clamp(0, 255) ]
+							when 3 then [ l.*(2).to_i.clamp(0, 255), 255.-(l).to_i.clamp(0, 255), 100.+(l / 2).to_i.clamp(0, 255) ]
+							when 4 then [ 30, 255.-(l / 2).to_i.clamp(0, 255), 110.+(l / 2).to_i.clamp(0, 255) ]
+							when 5 then [ 255.-(l * 2).to_i.clamp(0, 255), l.to_i.clamp(0, 255), 200 ]
+							when 6 then [ 50.+(255 - l).to_i.clamp(0, 255), 255.-(l / 2).to_i.clamp(0, 255), (l * 2).to_i.clamp(0, 255) ]
+							else [ l.*(2).to_i.clamp(0, 255), 255.-(l).to_i.clamp(0, 255), 100.+(l / 2).to_i.clamp(0, 255) ]
+						end
+					)
+				end
+
+				colours.reverse! if flip
+			end
+
+			i = -1
+			temp.concat "\e[38;2;#{colours[i][0]};#{colours[i][1]};#{colours[i][2]}m#{c[i]}" while (i += 1) < n
+		end
+
+		temp << "\e[0m".freeze
+	end
+end
+
 # Check which conflicting argument (e.g., `-md -html` together) is passed last
 # Always use the last argument
 conflicting, hash = [
@@ -54,7 +94,6 @@ execute.sort.each do |c|
 	next if e.class != Module && e.class != Class
 
 	meths = e.methods(false).sort
-	next if meths.any? { |a| e.method(a).arity > 0 }
 
 	if meths.length > 0
 		if MARKDOWN
@@ -66,21 +105,56 @@ execute.sort.each do |c|
 		end
 	end
 
+
 	meths.each do |meth|
+
+		arg = nil
+		params = e.method(meth).parameters
+
+		param = ''
+
+		params.each do |p|
+
+			case p[0]
+				when :opt
+					param << "#{p[1]}, "
+				when :key
+					param << ":#{p[1]}, "
+				when :req
+					param << p[1].to_s
+			end
+		end
+
+		param.delete_suffix!(", ")
+
+		if e.method(meth).arity > 0
+			if c == :PrettifyBytes
+				arg = rand(10 ** 15)
+			else
+				next
+			end
+		end
+
+		disp_meth = "#{meth}"
+		disp_meth.concat(arg ? "(#{param} = #{arg})" : "(#{param})")
+
 		time = Time.now
-		v = e.send(meth)
+		v = arg ? e.send(meth, arg) : e.send(meth)
 		time2 = Time.now
 		time = time2.-(time).*(1_000_000).round(3)
 
 		v = v.inspect
 		dis = v.length > 253 ? v[0..250].strip + '...'.freeze : v
 
+		source = e.singleton_method(meth).source_location.to_a
+		src = source.empty? ? ''.freeze : "\u2B23 file: #{File.split(source[0])[-1]}, line: #{source[1]}\n"
+
 		if MARKDOWN
-			puts "#{e}.#{meth}\n=> #{dis}"
+			puts "#{src}#{e}.#{disp_meth}\n=> #{dis}"
 		elsif HTML
-			puts "#{e}.#{meth}\n=> #{dis}"
+			puts "#{src}#{e}.#{disp_meth}\n=> #{dis}"
 		else
-			puts "\e[1;38;2;80;80;255m#{e}.#{meth}\e[0m\n=> #{dis}"
+			puts "#{src.colourize}\e[1;38;2;80;80;255m#{e}.#{disp_meth}\e[0m\n=> #{dis}"
 		end
 
 		puts( "(" +

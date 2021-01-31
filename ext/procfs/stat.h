855a60a6b21b355da14d09691c8f8d45a59e6dc4
Sourav Goswami <souravgoswami@protonmail.com>
2021-02-01 02:24:09 +0530

    Read stat properly

--- ext/procfs/stat.h
+++ ext/procfs/stat.h
@@ -17,33 +17,76 @@ VALUE ps_state(VALUE obj, VALUE pid) {
 VALUE ps_stat(VALUE obj, VALUE pid) {
 	char _path[22] ;
 	sprintf(_path, "/proc/%lu/stat", FIX2UINT(pid)) ;
-	VALUE ary = rb_ary_new() ;
 
 	FILE *f = fopen(_path, "r") ;
 
 	if (!f)
-		return ary ;
+		return rb_ary_new() ;
 
-	unsigned int buf_size = 1000 ;
+	// ?? JEEZ !!
+	// We need to do this because the datatypes are different
+	// Can't make an array like Ruby out of String
+	// This is hard to write and check, but tested, and works.
+	//
+	// For this struct,
+	// follow https://man7.org/linux/man-pages/man5/proc.5.html
+	int ppid, pgrp, session, tty_nr, tpgid ;
+	unsigned flags ;
+	long unsigned minflt, cminflt, majflt, cmajflt, utime, stime ;
+	long cutime, cstime, priority, nice, num_threads, itrealvalue ;
+	long long unsigned starttime ;
+	long unsigned vsize ;
+	long rss ;
+	long unsigned rsslim, startcode, endcode, startstack, kstkesp, kstkeip ;
+	long unsigned signal, blocked, sigignore, sigcatch, wchan, nswap, cnswap ;
+	int exit_signal, processor ;
+	unsigned rt_priority, policy ;
+	long long unsigned delayacct_blkio_ticks ;
+	long unsigned guest_time ;
+	long cguest_time ;
+	long unsigned start_data, end_data, start_brk, arg_start, arg_end ;
+	long unsigned env_start, env_end ;
+	int exit_code ;
 
-	char s[buf_size] ;
-	char status = fscanf(f, "%*s (%*16[^)]) %*s ") ;
+	char status = fscanf(
+		f, "%*d (%*16[^)]) %*c "
+		"%d %d %d %d %d %u "
+		"%lu %lu %lu %lu %lu %lu "
+		"%ld %ld %ld %ld %ld %ld "
+		"%llu %lu %ld %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu "
+		"%d %d %u %u %llu %lu %ld %lu %lu %lu %lu %lu %lu %lu %d", &ppid, &pgrp,
+		&session, &tty_nr, &tpgid, &flags, &minflt, &cminflt, &majflt, &cmajflt, &utime, &stime, &cutime,
+		&cstime, &priority, &nice, &num_threads, &itrealvalue, &starttime, &vsize, &rss, &rsslim,
+		&startcode, &endcode, &startstack, &kstkesp, &kstkeip, &signal, &blocked, &sigignore,
+		&sigcatch, &wchan, &nswap, &cnswap, &exit_signal, &processor, &rt_priority, &policy,
+		&delayacct_blkio_ticks, &guest_time, &cguest_time, &start_data, &end_data,
+		&start_brk, &arg_start, &arg_end, &env_start, &env_end, &exit_code
+	) ;
 
-	if (status != 0)
-		return ary ;
+	fclose(f) ;
 
-	if(fgets(s, buf_size, f) == NULL)
-		return ary ;
+	if (status != 49)
+		return rb_ary_new() ;
 
-	char *token = strtok(s, " ") ;
-	unsigned long long converted_int ;
-
-	while (token != NULL) {
-		converted_int = strtoull(token, NULL, 10) ;
-		rb_ary_push(ary, ULL2NUM(converted_int)) ;
-
-		token = strtok(NULL, " ") ;
-	}
-
-	return ary ;
+	return rb_ary_new_from_args(49,
+	INT2NUM(ppid), INT2NUM(pgrp), INT2NUM(session), INT2NUM(tty_nr), INT2NUM(tpgid),
+	UINT2NUM(flags),
+	ULONG2NUM(minflt), ULONG2NUM(cminflt), ULONG2NUM(majflt), ULONG2NUM(cmajflt),
+	ULONG2NUM(utime), ULONG2NUM(stime),
+	LONG2NUM(cutime), LONG2NUM(cstime), LONG2NUM(priority),
+	LONG2NUM(nice), LONG2NUM(num_threads), LONG2NUM(itrealvalue),
+	ULL2NUM(starttime),
+	ULONG2NUM(vsize),
+	LONG2NUM(rss),
+	ULONG2NUM(rsslim), ULONG2NUM(startcode), ULONG2NUM(endcode),
+	ULONG2NUM(startstack), ULONG2NUM(kstkesp), ULONG2NUM(kstkeip),
+	ULONG2NUM(signal), ULONG2NUM(blocked), ULONG2NUM(sigignore), ULONG2NUM(sigcatch),
+	ULONG2NUM(wchan), ULONG2NUM(nswap), ULONG2NUM(cnswap),
+	INT2NUM(exit_signal), INT2NUM(processor),
+	UINT2NUM(rt_priority), UINT2NUM(policy),
+	ULL2NUM(delayacct_blkio_ticks), ULONG2NUM(guest_time), LONG2NUM(cguest_time), ULONG2NUM(start_data),
+	ULONG2NUM(end_data), ULONG2NUM(start_brk), ULONG2NUM(arg_start), ULONG2NUM(arg_end),
+	ULONG2NUM(env_start), ULONG2NUM(env_end),
+	INT2NUM(exit_code)
+	) ;
 }

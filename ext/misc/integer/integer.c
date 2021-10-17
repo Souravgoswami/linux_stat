e2b2fa7b8953f1a9fa4a19c9a7539baf33817c29
Sourav <souravgoswami@pm.me>
2021-10-17 22:16:31 +0530

    Slight performance improvements

--- ext/misc/integer/integer.c
+++ ext/misc/integer/integer.c
@@ -1,35 +1,42 @@
+#if defined(__GNUC__) && !defined(__clang__) && !defined(__INTEL_COMPILER)
+	#pragma GCC optimize ("O3")
+	#pragma GCC diagnostic warning "-Wall"
+#elif defined(__clang__)
+	#pragma clang optimize on
+	#pragma clang diagnostic warning "-Wall"
+#elif defined(__INTEL_COMPILER)
+	#pragma intel optimization_level 3
+#endif
+
 #include "ruby.h"
+#include "limits.h"
 
 VALUE isNumber(volatile VALUE obj, volatile VALUE val) {
 	// If the argument is T_FIXNUM or T_BIGNUM, they are of course, Integer
 	// But we don't expect anything other than String though as Argument.
 	// Note that raising ArgumentError or any kind of Error shouldn't be done here
 	// Otherwise Integer(n) is the best method in Ruby.
-	if (RB_TYPE_P(val, T_FIXNUM) || RB_TYPE_P(val, T_BIGNUM)) {
+	if (RB_TYPE_P(val, T_FIXNUM) || RB_TYPE_P(val, T_BIGNUM))
 		return Qtrue ;
-	} else if (!RB_TYPE_P(val, T_STRING)) {
+
+	if (!RB_TYPE_P(val, T_STRING))
 		return Qnil ;
-	}
 
 	char *str = StringValuePtr(val) ;
 
 	// If the string is empty, return false
-	if (!str[0]) {
-		return Qfalse ;
-	}
+	if (!str[0]) return Qfalse ;
 
 	unsigned char i = 0 ;
 	unsigned char max = UCHAR_MAX ;
 	char ch ;
 
+	# pragma GCC unroll 4
 	while((ch = str[i++])) {
-		if (ch < 48 || ch > 57) {
+		if (ch < 48 || ch > 57)
 			return Qfalse ;
-		}
 
-		if (i == max) {
-			return Qnil ;
-		}
+		if (i == max) return Qnil ;
 	}
 
 	return Qtrue ;

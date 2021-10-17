52b9352ae0c039d55a0545a122510b905a85bb1e
Sourav <souravgoswami@pm.me>
2021-10-17 18:44:18 +0530

    Return false when string is empty

--- ext/misc/integer/integer.c
+++ ext/misc/integer/integer.c
@@ -1,6 +1,10 @@
 #include "ruby.h"
 
 VALUE isNumber(volatile VALUE obj, volatile VALUE val) {
+	// If the argument is T_FIXNUM or T_BIGNUM, they are of course, Integer
+	// But we don't expect anything other than String though as Argument.
+	// Note that raising ArgumentError or any kind of Error shouldn't be done here
+	// Otherwise Integer(n) is the best method in Ruby.
 	if (RB_TYPE_P(val, T_FIXNUM) || RB_TYPE_P(val, T_BIGNUM)) {
 		return Qtrue ;
 	} else if (!RB_TYPE_P(val, T_STRING)) {
@@ -8,6 +12,12 @@ VALUE isNumber(volatile VALUE obj, volatile VALUE val) {
 	}
 
 	char *str = StringValuePtr(val) ;
+
+	// If the string is empty, return false
+	if (!str[0]) {
+		return Qfalse ;
+	}
+
 	unsigned char i = 0 ;
 	unsigned char max = UCHAR_MAX ;
 	char ch ;

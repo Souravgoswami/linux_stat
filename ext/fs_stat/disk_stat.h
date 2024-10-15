7c9e1c655c4a4383ba07f448320164df039e9e19
Souravgoswami <souravgoswami@protonmail.com>
2024-10-16 03:07:26 +0530

    Fix potential buffer overflow in getDiskStats by limiting sscanf string length and adding safety checks.

--- ext/fs_stat/disk_stat.h
+++ ext/fs_stat/disk_stat.h
@@ -6,17 +6,19 @@ static VALUE getDiskStats (VALUE obj, VALUE path) {
 	unsigned long long read, write ;
 	char *p = StringValuePtr(path) ;
 
-	while(fgets(lines, 119, file)) {
-		sscanf(lines, "%*s %*s %s %*s %*s %llu %*s %*s %*s %llu", lines, &read, &write) ;
+	while (fgets(lines, sizeof(lines) - 1, file)) {
+		if (sscanf(lines, "%*s %*s %119s %*s %*s %llu %*s %*s %*s %llu", lines, &read, &write) == 3) {
+			if(strcmp(lines, p) == 0) {
+				fclose(file) ;
 
-		if(strcmp(lines, p) == 0) {
-			fclose(file) ;
-
-			return rb_ary_new_from_args(
-				2,
-				ULL2NUM(read),
-				ULL2NUM(write)
-			) ;
+				return rb_ary_new_from_args(
+					2,
+					ULL2NUM(read),
+					ULL2NUM(write)
+				) ;
+			}
+		} else {
+			return rb_ary_new();
 		}
 	}
 
